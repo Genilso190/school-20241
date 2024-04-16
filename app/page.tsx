56726b42da60bcd7dfc7e548dcf3a030915f4a4e
Genilso Carraro <71860731+Genilso190@users.noreply.github.com>
2024-04-16 23:28:12 +0000

    fix: Página Inicial

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-center">Pagina Inicial 2</h1>
+    <h1 className="text-center">ESCOLA TECNOLÓGICA</h1>
   );
 }
