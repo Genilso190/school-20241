d079813430e98b9d1c6e3343c6af8ceacfe8ed60
Genilso Carraro <71860731+Genilso190@users.noreply.github.com>
2024-04-16 23:31:55 +0000

    fix:atualizada

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-center">ESCOLA TECNOLÓGICA</h1>
+    <h1 className="text-center">ESCOLA TECNOLÓGICA ATUALIZADA</h1>
   );
 }
 
