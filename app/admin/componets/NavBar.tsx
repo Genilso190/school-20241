b2e32d85a9f901a32c209f8d57d2717654f7d8b2
Genilso Carraro <71860731+Genilso190@users.noreply.github.com>
2024-05-08 01:12:59 +0000

    fix: avatar

--- app/admin/componets/NavBar.tsx
+++ app/admin/componets/NavBar.tsx
@@ -1,10 +1,14 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 export default function NavBar(){
     return(
         <div className=" flex justify-between p-6 border-b-2">
             <h1>Logo</h1>
             <a href="/admin">Home</a>
             <a href="/admin/student">Navegar Estudante</a>
-            <h1>Avatar</h1>
+            <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
 
         </div>
 
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,19 +1,15 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
 import { Button } from "@/components/ui/button"
 
 export default function () {
     return (
         <div>
 
-        <h1 className="">Bem vindo a Area Administrativa</h1>
-        <Button>Salvar</Button>
-        <Button variant="destructive">Excluir</Button>
-        <Button variant="secundary">Novo</Button>
-        <Avatar>
-    <AvatarImage src="https://github.com/shadcn.png" />
-    <AvatarFallback>CN</AvatarFallback>
-</Avatar>
-
+            <h1 className="">Bem vindo a Area Administrativa</h1>
+            <Button>Salvar</Button>
+            <Button variant="destructive">Excluir</Button>
+            <Button variant="secundary">Novo</Button>
+          
 
         </div>
     )
