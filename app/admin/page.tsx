bccf8401e963e0d0f635e2a0e78f32cd02ca60a5
Genilso Carraro <71860731+Genilso190@users.noreply.github.com>
2024-05-22 00:38:49 +0000

    fix: correção de bug

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,16 +2,16 @@
 import { Button } from "@/components/ui/button"
 import { MenuSheet } from "./componets/MenuSheet"
 
-export default function () {
+export default function Admin () {
     return (
         <div>
 
-            <h1 className="">Bem vindo a Area Administrativa</h1>
+            <h1 className="text-center">Bem vindo a Area Administrativa</h1>
             <Button>Salvar</Button>
             <Button variant="destructive">Excluir</Button>
             <Button variant="secondary">Novo</Button>
 
-          <MenuSheet/>
+            <MenuSheet />
 
         </div>
     )
